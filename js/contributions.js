d2b1081ea9864e823c06daeb6503310af72db5e7
olivia_r_brown <olivia_r_brown@apple.com>
2018-06-15 15:51:58 -0700

    Link to user profile

--- js/contributions.js
+++ js/contributions.js
@@ -2,15 +2,13 @@
   restoreUserName()
   const chart = document.querySelector('img.chart');
   const title = document.querySelector('h1.title');
-    const userLink = document.querySelector('a.userLink');
 
   function restoreUserName() {
     chrome.storage.sync.get({
       userName: 'oliviabrown9',
     }, function(items) {
-      title.innerHTML = items.userName + "'s GitHub Contributions"
-      userLink.href = "https:github.com/" + items.userName
-      console.log(userLink.href)
+      const userLink = 'https:github.com/' + items.userName
+      title.innerHTML = "<a href=" + userLink + ">" + items.userName +"'s' GitHub Contributions</a>"
       chart.src = "http://ghchart.rshah.org/" + items.userName
       return items.userName
     });
