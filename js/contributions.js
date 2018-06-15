d55edbb213d87b1528c1d7f6f11e065093c67aad
olivia_r_brown <olivia_r_brown@apple.com>
2018-06-15 15:52:26 -0700

    Remove extra quote

--- js/contributions.js
+++ js/contributions.js
@@ -8,7 +8,7 @@
       userName: 'oliviabrown9',
     }, function(items) {
       const userLink = 'https:github.com/' + items.userName
-      title.innerHTML = "<a href=" + userLink + ">" + items.userName +"'s' GitHub Contributions</a>"
+      title.innerHTML = "<a href=" + userLink + ">" + items.userName +"'s GitHub Contributions</a>"
       chart.src = "http://ghchart.rshah.org/" + items.userName
       return items.userName
     });
