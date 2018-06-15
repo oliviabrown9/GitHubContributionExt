ef08543530d4c8ef5a7acd707e0c8030b82047c4
olivia_r_brown <olivia_r_brown@apple.com>
2018-06-15 14:19:59 -0700

    Create settings site

---
+++ js/settings.js
@@ -0,0 +1,11 @@
+(function() {
+  console.log("hello")
+  console.log(saveButton)
+  const saveButton = document.querySelector('button.save.button-default')
+
+
+  saveButton.addEventListener('click', event => {
+    const userName = $(input[name="username"]).val();
+    console.log(userName)
+  });
+  });
