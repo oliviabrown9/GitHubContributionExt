47c10524841284af3f8c5572033de095643e07c0
Olivia <obrown917@gmail.com>
2018-06-18 22:23:20 -0700

    Add color selection support to Chrome

--- Chrome/js/settings.js
+++ Chrome/js/settings.js
@@ -1,22 +1,39 @@
 (function() {
   const saveButton = document.querySelector('button.save')
   const userNameInput = document.querySelector('input[name="username"]')
+  const colorInput = document.querySelector('input[name="color"]')
   const message = document.querySelector('h5.message');
   restoreSettings()
 
   saveButton.addEventListener('click', event => {
-    const userName = userNameInput.value
+    var userName
+    if (userNameInput.value) {
+      userName = userNameInput.value
+    }
+    else {
+      userName = userNameInput.placeholder
+    }
+    var color
+    if (colorInput.value) {
+      color = colorInput.value
+    }
+    else {
+      color = colorInput.placeholder
+    }
     chrome.storage.sync.set({
       userName: userName,
+      color: color
     })
     message.innerHTML = "Settings saved :)"
   });
 
   function restoreSettings() {
   chrome.storage.sync.get({
-    userName: 'oliviabrown9',
+    userName: '',
+    color: ''
   }, function(items) {
     userNameInput.placeholder = items.userName
+    colorInput.placeholder = items.color
   });
 }
 
